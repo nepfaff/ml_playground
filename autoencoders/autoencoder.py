24ff99b9412bf657153c8722cb8233f40adc6fcc
Nicholas Pfaff <contact@nicholaspfaff.ch>
2022-06-19 09:36:00 +0100

    Add CNN autoencoder model

--- autoencoders/autoencoder.py
+++ autoencoders/autoencoder.py
@@ -53,5 +53,57 @@ def get_dense_mnist_autoencoder(encoder_dropout: float = 0.0, decoder_dropout: f
     return Model(encoder_dropout, decoder_dropout)
 
 
-def get_cnn_mnist_autoencoder() -> nn.Module:
-    pass
+def get_cnn_mnist_autoencoder(encoder_dropout: float = 0.0, decoder_dropout: float = 0.0) -> nn.Module:
+    """
+    :param encoder_dropout: Amount of dropout to add after each encoder layer in range [0,1].
+    :param decoder_dropout: Amount of dropout to add after each decoder layer in range [0,1].
+    """
+
+    class Model(nn.Module):
+        def __init__(self, encoder_dropout: float, decoder_dropout: float):
+            super(Model, self).__init__()
+
+            self._encoder = nn.Sequential(
+                nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1),  # Output dim = (batch_size, 32, 14, 14)
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+                nn.Dropout(encoder_dropout),
+                nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),  # Output dim = (batch_size, 64, 7, 7)
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.Dropout(encoder_dropout),
+                nn.Conv2d(64, 128, kernel_size=3, stride=2),  # Output dim = (batch_size, 128, 3, 3)
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                nn.Dropout(encoder_dropout),
+                nn.Flatten(1),  # Output dim = (batch_size, 1152)
+                nn.Linear(1152, 32),
+                nn.ReLU(),
+                nn.Dropout(decoder_dropout),
+            )
+
+            self._decoder = nn.Sequential(
+                nn.Linear(32, 1152),
+                nn.ReLU(),
+                nn.Dropout(decoder_dropout),
+                nn.Unflatten(1, (128, 3, 3)),  # Output dim = (batch_size, 128, 3, 3)
+                nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2),  # Output dim = (batch_size, 64, 7, 7)
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.Dropout(encoder_dropout),
+                nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1),  # Output dim = (batch_size, 32, 13, 13)
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+                nn.Dropout(encoder_dropout),
+                nn.ConvTranspose2d(
+                    32, 1, kernel_size=3, stride=2, output_padding=1
+                ),  # Output dim = (batch_size, 1, 28, 28)
+                nn.Sigmoid(),  # Want output values in range [0,1] (same as input)
+            )
+
+        def forward(self, x):
+            x = self._encoder(x)
+            x = self._decoder(x)
+            return x
+
+    return Model(encoder_dropout, decoder_dropout)
